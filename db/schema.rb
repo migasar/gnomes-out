a330b7d2d9f1278c7160670b4ee0eb1a80c9710f
Estelle <estelle.cirilo@gmail.com>
2021-08-16 16:58:02 +0200

    all model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_143709) do
+ActiveRecord::Schema.define(version: 2021_08_16_145639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "gnome_id", null: false
+    t.date "date_start"
+    t.date "date_end"
+    t.float "price_total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gnome_id"], name: "index_bookings_on_gnome_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "gnomes", force: :cascade do |t|
+    t.string "name"
+    t.float "size"
+    t.float "weight"
+    t.string "state"
+    t.string "category"
+    t.string "gender"
+    t.string "mood"
+    t.boolean "outsider"
+    t.boolean "traveler"
+    t.boolean "available"
+    t.float "price"
+    t.string "color"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_gnomes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +62,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_143709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "gnomes"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "gnomes", "users"
 end
