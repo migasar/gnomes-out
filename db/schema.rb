a1b985f2d722d5605797c6fe0229f60cf93a1a8d
anna bozio <anna.bozio@gmail.com>
2021-08-17 11:28:17 +0200

    add photo to gnomes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_145639) do
+ActiveRecord::Schema.define(version: 2021_08_17_092531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_145639) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_gnomes_on_user_id"
   end
 
