ae03f82757fb7ea80674224b6151ea372309bd5c
Anne Barois <barois.anne@orange.fr>
2021-08-17 17:13:09 +0200

    Add new model Favorites and correction of seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_092531) do
+ActiveRecord::Schema.define(version: 2021_08_17_150642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_08_17_092531) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["gnome_id"], name: "index_bookings_on_gnome_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "gnome_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gnome_id"], name: "index_favorites_on_gnome_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "gnomes", force: :cascade do |t|
@@ -65,5 +74,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_092531) do
 
   add_foreign_key "bookings", "gnomes"
   add_foreign_key "bookings", "users"
+  add_foreign_key "favorites", "gnomes"
+  add_foreign_key "favorites", "users"
   add_foreign_key "gnomes", "users"
 end
