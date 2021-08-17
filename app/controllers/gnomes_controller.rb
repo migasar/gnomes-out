a50834879c1ea4b20cb8acd4cb7e52f7e2b13823
anna bozio <anna.bozio@gmail.com>
2021-08-17 11:54:52 +0200

    delete the text at the end

--- app/controllers/gnomes_controller.rb
+++ app/controllers/gnomes_controller.rb
@@ -23,17 +23,3 @@ class GnomesController < ApplicationController
     params.require(:gnome).permit(:name, :photo, :size, :weight, :state, :category, :gender, :mood, :outsider, :traveler, :available, :price, :color, :user_id )
   end
 end
-
- t.string "name"
-    t.float "size"
-    t.float "weight"
-    t.string "state"
-    t.string "category"
-    t.string "gender"
-    t.string "mood"
-    t.boolean "outsider"
-    t.boolean "traveler"
-    t.boolean "available"
-    t.float "price"
-    t.string "color"
-    t.bigint "user_id", null: false
