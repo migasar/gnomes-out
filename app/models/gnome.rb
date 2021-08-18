7aa0f302b0e8898ea19eeae3cb172d4f15812592
anna bozio <anna.bozio@gmail.com>
2021-08-18 14:43:49 +0200

    footer done

--- app/models/gnome.rb
+++ app/models/gnome.rb
@@ -1,13 +1,13 @@
 class Gnome < ApplicationRecord
-  
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   belongs_to :user
   has_many :favorites, dependent: :destroy
-  CATEGORY = %w[authentique zombie bdsm familial potache zen groupé mignon connu]
-  COLOR = %w[rouge vert blanc jaune orange bleu noir]
-  MOOD = %w[joyeux triste colérique détendu]
+  CATEGORY = %w[Authentique Zombie Bdsm Familial Potache Zen Groupé Mignon Connu]
+  COLOR = %w[Rouge Vert Blanc Jaune Orange Bleu Noir]
+  MOOD = %w[Joyeux Triste Colérique Détendu]
 
   validates :name, :price, :photo, presence: true
   validates :category, inclusion: { in: CATEGORY }
