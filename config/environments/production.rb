a78b9a98c1aec91cef8eb52e47ee2f668ac3258f
Estelle <estelle.cirilo@gmail.com>
2021-10-12 13:48:34 +0200

    change-host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  server '152.228.162.169', user: 'ubuntu', roles: %w{app db web}
 end
